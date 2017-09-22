816b9663614037479f8f6e7d2161ce412b9bba79
Nikos <nikos.bre@gmail.com>
2017-09-22 11:37:00 -0700

    Update vagrant-up.sh

--- vagrant-up.sh
+++ vagrant-up.sh
@@ -3,7 +3,5 @@
 export K8S_NODE_OS=ubuntu
 export K8S_NODES=1
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
-#export HTTP_PROXY=http://proxy-wsa.esl.cisco.com:80 
-#export HTTPS_PROXY=http://proxy-wsa.esl.cisco.com:80 
 
 vagrant up 
